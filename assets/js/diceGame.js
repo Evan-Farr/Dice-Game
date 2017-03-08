b369073b36df891dfaf227640ca85d20520108ce
Evan-Farr <evan.c.farr@gmail.com>
2017-03-08 16:16:39 -0600

    renamed id names for readablity

--- assets/js/diceGame.js
+++ assets/js/diceGame.js
@@ -1,25 +1,25 @@
 "use strict";
 
 function hitDriver() {
-  document.getElementById('dieDriver').textContent = (Math.floor(Math.random() * (20 - 12 + 1)) + 12);
+  document.getElementById('diceDriver').textContent = (Math.floor(Math.random() * (20 - 12 + 1)) + 12);
 }
 
 function hitHybrid() {
-  document.getElementById('dieHybrid').textContent = (Math.floor(Math.random() * (12 - 1 + 1)) + 1);
+  document.getElementById('diceHybrid').textContent = (Math.floor(Math.random() * (12 - 1 + 1)) + 1);
 }
 
 function hitFourIron() {
-  document.getElementById('dieFourIron').textContent = (Math.floor(Math.random() * (10 - 1 + 1)) + 1);
+  document.getElementById('diceFourIron').textContent = (Math.floor(Math.random() * (10 - 1 + 1)) + 1);
 }
 
 function hitSevenIron() {
-  document.getElementById('dieSevenIron').textContent = (Math.floor(Math.random() * (8 - 3 + 1)) + 3);
+  document.getElementById('diceSevenIron').textContent = (Math.floor(Math.random() * (8 - 3 + 1)) + 3);
 }
 
 function hitWedge() {
-  document.getElementById('dieWedge').textContent = (Math.floor(Math.random() * (6 - 1 + 1)) + 1);
+  document.getElementById('diceWedge').textContent = (Math.floor(Math.random() * (6 - 1 + 1)) + 1);
 }
 
 function hitPutter() {
-  document.getElementById('diePutter').textContent = (Math.floor(Math.random() * (4 - 1 + 1)) + 1);
+  document.getElementById('dicePutter').textContent = (Math.floor(Math.random() * (4 - 1 + 1)) + 1);
 }
