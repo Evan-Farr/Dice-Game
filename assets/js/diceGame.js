b31021d043304bf920ab59f6ddcf576cc0bba7c4
Evan-Farr <evan.c.farr@gmail.com>
2017-03-09 10:11:13 -0600

    starting scorecard

--- assets/js/diceGame.js
+++ assets/js/diceGame.js
@@ -1,25 +1,49 @@
 "use strict";
 
+function startGame() {
+  var welcomeMessage = alert("Welcome to Yipes!\nThis is a simple, easy to play and addictive competitive virtual dice-rolling golf game.\nPlayer count: 1-4\nGood Luck!");
+}
+
+function countTotalHits() {
+  var hitCount = 0;
+}
+
+function respondToHit() {
+  var msg = "";
+}
+
 function hitDriver() {
-  document.getElementById('diceDriver').textContent = (Math.floor(Math.random() * (20 - 12 + 1)) + 12);
+  var linkToID = document.getElementById('diceDriver');
+  var randomNumberGen = Math.floor(Math.random() * (20 - 12 + 1)) + 12;
+  linkToID.textContent = randomNumberGen;
 }
 
 function hitHybrid() {
-  document.getElementById('diceHybrid').textContent = (Math.floor(Math.random() * (12 - 1 + 1)) + 1);
+  var linkToID = document.getElementById('diceHybrid');
+  var randomNumberGen = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+  linkToID.textContent = randomNumberGen;
 }
 
 function hitFourIron() {
-  document.getElementById('diceFourIron').textContent = (Math.floor(Math.random() * (10 - 1 + 1)) + 1);
+  var linkToID = document.getElementById('diceFourIron');
+  var randomNumberGen = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+  linkToID.textContent = randomNumberGen;
 }
 
 function hitSevenIron() {
-  document.getElementById('diceSevenIron').textContent = (Math.floor(Math.random() * (8 - 3 + 1)) + 3);
+  var linkToID = document.getElementById('diceSevenIron');
+  var randomNumberGen = Math.floor(Math.random() * (8 - 3 + 1)) + 3;
+  linkToID.textContent = randomNumberGen;
 }
 
 function hitWedge() {
-  document.getElementById('diceWedge').textContent = (Math.floor(Math.random() * (6 - 1 + 1)) + 1);
+  var linkToID = document.getElementById('diceWedge');
+  var randomNumberGen = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+  linkToID.textContent = randomNumberGen;
 }
 
 function hitPutter() {
-  document.getElementById('dicePutter').textContent = (Math.floor(Math.random() * (4 - 1 + 1)) + 1);
+  var linkToID = document.getElementById('dicePutter');
+  var randomNumberGen = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+  linkToID.textContent = randomNumberGen;
 }
