c4c6b948bd42971743c79544b14c346b81b4c8ce
Evan-Farr <evan.c.farr@gmail.com>
2017-03-10 17:09:16 -0600

    finished console based game!

--- assets/js/diceGame4.js
+++ assets/js/diceGame4.js
@@ -1,22 +1,31 @@
 "use strict";
 
-function play() {
-
-}
 
 function playHole(goal) {
   var strokes = 0;
-  var yardsHit = 0;
+  var totalYardsHit = 0;
+  var goal = 200;
+  var yardsRemaining = goal;
 
-  for (yardsHit !== goal) {
-    prompt("You have a distance of " + totalYards + " remaining to the hole.");
+  while (totalYardsHit !== goal) {
+    alert("The hole is currently " + yardsRemaining + " yards away.");
 
-    chooseClub();
-    yardsHit = swing(chooseClub());
+    var yardsHit = swing();
+    alert("You hit that ball " + yardsHit + " yards.");
+    totalYardsHit = totalYardsHit + yardsHit;
+    yardsRemaining = yardsRemaining - yardsHit;
+
+    if (totalYardsHit > goal) {
+      alert("Yips! Too much power! The ball must land exactly in the hole. Try again.");
+      swing();
+      totalYardsHit = totalYardsHit - yardsHit;
+      yardsRemaining = yardsRemaining + yardsHit;
+    }
 
     strokes++;
   }
-  return strokes;
+
+  alert("You finished this hole in " + strokes + " stroke! Refresh the page to start over.");
 }
 
 function chooseClub(){
@@ -26,21 +35,28 @@ function chooseClub(){
       return 20;
     case "4 iron":
       return 12;
-    case "hybrid":
-      return 10;
-    case "7 iron":
-      return 8;
-    case "wedge":
-      return 6;
     case "putter":
+      return 10;
+    case "hybrid":
+      return 8;
+    case "7 iron":
+      return 6;
+    case "wedge":
       return 4;
     default:
+      alert("Yips! You did not enter a valid club type. Please try again.");
       return chooseClub();
   }
 }
 
 function swing(club) {
   var club = chooseClub();
-  var yardsHit = Math.floor(Math.random() * (club + 1)) * 10;
+  var multiplier = 10;
+  if (club === 10) {
+    multiplier = 1;
+  }
+  var yardsHit = (Math.floor(Math.random() * club) + 1) * multiplier;
   return yardsHit;
 }
+
+playHole();
